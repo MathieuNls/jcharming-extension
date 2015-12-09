4333022583822520bb7c4287026dd361aab6f91d
Mathieu N <mathieu.nayrolles@gmail.com>
2015-12-09 15:14:32 -0500

    review typos continued

--- test.java
+++ test.java
@@ -3,8 +3,8 @@ public class {% SUT %}_{% BUG %} extends TestCase {
 	private static final String faillure = {% CRASH_STACK %};
 	private static final int threshold = {% THRESHOLD %};
 	private static int differences = Integer.MAX_VALUE;
-	private static final StringTokenizer tokenizerFaillure = 
-		new StringTokenizer(faillure, "\n");
+	private static final StringTokenizer tokenizerFailure =
+		new StringTokenizer(failure, "\n");
 
 	@Test
 	public test{% SUT %}() {
@@ -13,7 +13,7 @@ public class {% SUT %}_{% BUG %} extends TestCase {
 		} catch (Exception e) {
 			 // Count the differences
 		}
-		assertTrue(differences <= tokenizeOriginalFaillure
-			.countTokens() / 100 * (threshold-100));		 
+		assertTrue(differences <= tokenizeOriginalFailure
+			.countTokens() / 100 * (threshold-100));
 	}
 }
