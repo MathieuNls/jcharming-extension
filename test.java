1f874f1c18116f7c3420e407c86a11a024f43d3a
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-01-10 16:28:10 -0500

    End of revision - round 1

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class {% SUT %}_{% BUG %} extends TestCase {
 
-	private static final String faillure = {% CRASH_STACK %};
+	private static final String failure = {% CRASH_STACK %};
 	private static final int threshold = {% THRESHOLD %};
 	private static int differences = Integer.MAX_VALUE;
 	private static final StringTokenizer tokenizerFailure =
